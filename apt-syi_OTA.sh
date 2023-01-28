637b609c6c5bd66cb0771c0713d1c3738343f6fe
Ff-c109 <xjy20060326xjy@outlook.com>
2023-01-28 14:53:06 +0800

    OTA BUG fix

--- apt-syi_OTA.sh
+++ apt-syi_OTA.sh
@@ -2,7 +2,7 @@
 
 COUNTER=`curl -s https://raw.githubusercontent.com/Ff-c109/apt-syi/main/COUNTER`counter=`cat /opt/apt-syi/COUNTER`
 
-if [[ counter != COUNTER ]]
+if [[ $counter != $COUNTER ]]
 then
 	bash <(curl -s https://raw.githubusercontent.com/Ff-c109/apt-syi/main/apt-syi_installer-online.sh)
 	curl -s https://raw.githubusercontent.com/Ff-c109/apt-syi/main/COUNTER > /opt/apt-syi/COUNTER
