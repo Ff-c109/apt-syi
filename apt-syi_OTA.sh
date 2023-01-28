9ccd0f3b11051d77ffaac487a54955771a7fab22
Ff-c109 <xjy20060326xjy@outlook.com>
2023-01-28 15:03:23 +0800

    OTA: Bugfix

--- apt-syi_OTA.sh
+++ apt-syi_OTA.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-COUNTER=`curl -s https://raw.githubusercontent.com/Ff-c109/apt-syi/main/COUNTER`counter=`cat /opt/apt-syi/COUNTER`
+COUNTER=`curl -s https://raw.githubusercontent.com/Ff-c109/apt-syi/main/COUNTER`
 
-if [[ $counter != $COUNTER ]]
+counter=`cat /opt/apt-syi/COUNTER`
+
+if [[ $counter == $COUNTER ]]
 then
+	echo "OTA: do nothing."
+else
 	bash <(curl -s https://raw.githubusercontent.com/Ff-c109/apt-syi/main/apt-syi_installer-online.sh)
 	curl -s https://raw.githubusercontent.com/Ff-c109/apt-syi/main/COUNTER > /opt/apt-syi/COUNTER
-	echo "OAT: Auto fetch"
+	echo "OTA: Auto fetch"
 fi
 
